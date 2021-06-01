6bd4abd0c9fa55755144460e3cbdc6ed301bb927
Rahul Srivastava <rahul.srivastava12134@gmail.com>
2021-06-01 19:31:15 +0530

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".who-i-am", {
+    strings: ["Software Developer", "\nCompetetive Programmer", "Converting Caffine to Code xD"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    cursorChar: '',
+    loop: true
+  });
+  
